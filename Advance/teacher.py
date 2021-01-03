bba96b1ecd05ab2c7f9fa1369c88fb442c46a584
Abdullah Sajjad <76848653+abdullah-web-developer@users.noreply.github.com>
2021-01-03 18:39:42 +0500

    Create teacher.py

---
+++ Advance/teacher.py
@@ -0,0 +1,6 @@
+def teacher():
+    print("There are 3 teachers in the schools!")
+    print("Printing Output of __name__ == '__main__' from teacher() def is", __name__)
+
+if __name__ == "__main__":
+    teacher()
